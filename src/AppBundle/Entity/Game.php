2fca9cf72c8dc97da8a703c0ffffa8dab11d415e
rachid <rachidias@live.fr>
2018-08-29 13:54:05 +0200

    mise a jour

---
+++ src/AppBundle/Entity/Game.php
@@ -0,0 +1,18 @@
+<?php
+
+use Doctrine\ORM
+
+class Game {
+
+    /**
+     *
+     * @ORM
+     */
+    private $id;
+    private $created_date;
+    private $score;
+    private $type;
+    private $statut;
+    private $data;
+
+}
