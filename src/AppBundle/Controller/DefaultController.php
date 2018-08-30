ec3fc5099bf37e0c036bfe7fe499dd5cfbf8017d
elmahdy <aznet13@gmail.com>
2018-08-30 12:58:41 +0200

    mise a jour aziz

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('@App/Default/index.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscriptionAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/Default/inscription.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/game", name="game")
+     */
+    public function gameAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/Default/game.html.twig', [
         ]);
     }
 }
