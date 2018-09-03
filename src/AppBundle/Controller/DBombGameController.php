88749510198f4d322bcc5b5a84c5e0d827911771
rachid <rachidias@live.fr>
2018-09-03 08:45:55 +0200

    correction d'affichage de tableau

--- src/AppBundle/Controller/DBombGameController.php
+++ src/AppBundle/Controller/DBombGameController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -7,45 +8,23 @@ use Symfony\Component\Routing\Annotation\Route;
 use \AppBundle\Model\Board;
 
 class DBombGameController extends Controller {
-/**
+
+    /**
      * @Route("/jeu", name="game")
      */
     public function indexAction(Request $request) {
-        $typeGame='debutant';
-        $grid= [array()];
-  switch($typeGame){
-       
-   case 'debutant':
-       
-       $x=10;
-       $y=10;
-       for($i=0 ;$i<$x ;$i++){
-               for($j=0 ;$j<$y ;$j++){
-                   $grid[$i][$j]='';
-                       
-               }
-       
-       }
-           
-       break;  
-    case 'Intermidaire': 
-        
-           
-     case 'Avancé': 
-       
-       
-   }
-  
-  
-   $board=new Board;
-   $board->setGrid($grid);      
-       $aGrid=$board->getGrid(); 
+        $typeGame = 'debutant';
+
+        $board = new Board($typeGame);
 
         // replace this example code with whatever you need
         return $this->render('@App/DBomb/game.html.twig', [
-                    'grid' => $aGrid
+                    'board' => $board
         ]);
     }
 
-}
+    public function isWinAction() {
 
+    }
+
+}
--- src/AppBundle/Model/Board.php
+++ src/AppBundle/Model/Board.php
@@ -2,22 +2,48 @@
 
 namespace AppBundle\Model;
 
-class Board{
+class Board {
 
-private $grid;
+    private $grid = [];
 
+    public function __construct($typeGame) {
+        switch ($typeGame) {
 
- function getGrid() {
-     return $this->grid;
- }
+            case 'debutant':
+                $x = 10;
+                $y = 10;
+                break;
 
- function setGrid($grid) {
-     $this->grid = $grid;
- }
+            case 'Intermidaire':
+                $x = 15;
+                $y = 15;
+                break;
 
+            case 'Avancé':
+                $x = 20;
+                $y = 20;
+                break;
+        }
+
+        for ($i = 0; $i < $x; $i++) {
+            $this->grid[$i] = [];
+
+            for ($j = 0; $j < $y; $j++) {
+                $this->grid[$i][$j] = '';
+            }
+        }
+    }
+
+    public function init() {
+
+    }
+
+    function getGrid() {
+        return $this->grid;
+    }
+
+    function setGrid($grid) {
+        $this->grid = $grid;
+    }
 
 }
-    
-
-
-
