bd4f95bf72017cbb9a182cf9226f4c572a24b83f
Your Name <you@example.com>
2018-09-03 11:33:56 +0200

    création de class player

---
+++ src/AppBundle/Model/Player.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Model;
+
+class Player {
+
+    private $x;
+    private $y;
+    private $pointsVie=3; 
+    private $max_x = null;
+    private $max_y = null;
+
+    public function __construct($max_x, $max_y) {
+        $this->max_x = $max_x;
+        $this->max_y = $max_y;
+    }
+
+    public function isMovementPossible($x, $y) 
+  {
+    // case dans le tableau
+    if ($x < 0 || $y < 0 || $x > $max_x || $y > $max_y) {
+        return false;
+    }
+
+
+    // case haut
+    if ($y == $this->y-1 && $x == $this->x) {
+        return true;
+    }
+
+
+    // case pas
+    if ($y == $this->y+1 && $x == $this->x) {
+        return true;
+    }
+
+   // cas droite
+     if ($y == $this->y && $x == $this->x+1) {
+        return true;
+    }
+
+
+   // cas gauche
+     if ($y == $this->y && $x == $this->x-1) {
+        return true;
+    }
+
+  }
+
+    public function deplacer($x, $y) 
+      {
+            $this->x = $x;
+            $this->y = $y;
+       }  
+
+
+
+   public function bomb() 
+      {
+        $pointsVie-=$pointsVie; 
+      }
+                                             
+}
